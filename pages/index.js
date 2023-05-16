98e606a6a1f483c8198963c0183f6528a3e27474
Jordan Roberts <jrobertswebdev@gmail.com>
2023-05-16 11:13:43 -0500

    change open graph image

--- pages/index.js
+++ pages/index.js
@@ -28,10 +28,10 @@ export default function Home() {
           <meta property='og:title' content='Jordan Roberts' />
           <meta property='og:type' content='website' />
           <meta property='og:description' content='Jordan Roberts, web developer' />
-          <meta property='og:image' content='https://i.postimg.cc/25VnCHkr/port-title-fixed.png' />
+          <meta property='og:image' content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png' />
           <meta property='og:url' content='https://www.jrobertsweb.dev/' />
           <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:image' content='https://i.postimg.cc/25VnCHkr/port-title-fixed.png' />
+          <meta name='twitter:image' content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png' />
           <meta name='twitter:creator' content='@jRobertsWebDev' />
           <meta name='viewport' content='width=500' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />

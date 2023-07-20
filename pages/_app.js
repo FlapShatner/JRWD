99ef7d448f644ba985191666070cf0c3b2287a94
Jordan Roberts <jrobertswebdev@gmail.com>
2023-07-20 09:25:18 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Analytics } from '@vercel/analytics'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return (
+  <>
+   <Component {...pageProps} />
+   <Analytics />
+  </>
+ )
 }
 
 export default MyApp

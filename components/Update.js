d01718fd7831de5844fe9f9182fed8be6b1437f4
Jordan Roberts <jrobertswebdev@gmail.com>
2023-08-02 23:27:08 -0500

    add update banner

---
+++ components/Update.js
@@ -0,0 +1,23 @@
+const Update = () => {
+ return (
+  <div>
+   <h2>
+    You are looking at my old website. I have a new one! Check out <a href='https://v2.jrobertsweb.dev/'>v2.jrobertsweb.dev</a>!
+   </h2>
+   <style jsx>
+    {`
+     h2 {
+      font-size: 1.5rem;
+      font-family: var(--ff-nanum);
+      color: black;
+     }
+     div {
+      background-color: red;
+     }
+    `}
+   </style>
+  </div>
+ )
+}
+
+export default Update
--- pages/index.js
+++ pages/index.js
@@ -5,265 +5,316 @@ import Skills from '../components/Skills'
 import Footer from '../components/Footer'
 import { ModalWrap } from '../components/ModalWrap'
 import { Contact } from '../components/Contact'
+import Update from '../components/Update'
 import { useState, useRef } from 'react'
 
 export default function Home() {
-  const projectsRef = useRef(null)
-  const imageText = '</>'
-  const bio = 'I am a web developer / programming enthusiast who is obsessed with learning and honing my skills.'
+ const projectsRef = useRef(null)
+ const imageText = '</>'
+ const bio = 'I am a web developer / programming enthusiast who is obsessed with learning and honing my skills.'
 
-  const email = 'Jordan@jRobertsWeb.dev'
-  const [dark, setDark] = useState(false)
-  const [contact, setContact] = useState(false)
+ const email = 'Jordan@jRobertsWeb.dev'
+ const [dark, setDark] = useState(false)
+ const [contact, setContact] = useState(false)
 
-  const scrollTo = () => {
-    projectsRef.current?.scrollIntoView({ behavior: 'smooth' })
-  }
+ const scrollTo = () => {
+  projectsRef.current?.scrollIntoView({ behavior: 'smooth' })
+ }
 
-  return (
-    <>
-      <div className='wrapper'>
-        <Head>
-          <title>Jordan Roberts</title>
-          <meta property='og:title' content='Jordan Roberts' />
-          <meta property='og:type' content='website' />
-          <meta property='og:description' content='Jordan Roberts, full stack web developer specializing in React' />
-          <meta property='og:image' content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png' />
-          <meta property='og:url' content='https://www.jrobertsweb.dev/' />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:image' content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png' />
-          <meta name='twitter:creator' content='@jRobertsWebDev' />
-          <meta name='viewport' content='width=500' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
-          {/* <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' /> */}
-        </Head>
-        <div className='container'>
-          <Header dark={dark} contactOpen={() => setContact(!contact)} toggleDark={() => setDark(!dark)} scrollTo={scrollTo} />
-          <div className='boundary'>
-            <ModalWrap dark={dark} show={contact} close={() => setContact(!contact)} />
-          </div>
-          <div className='mobile'>
-            <h1>Jordan Roberts</h1>
-            <h2>Full stack web developer</h2>
-          </div>
-          <div className='main'>
-            <div className='col bio'>
-              <h1 className='desktop'>Jordan Roberts</h1>
-              <h2 className='desktop'>Full stack web developer</h2>
-              <div className='text'>
-                <p>{bio}</p>
-                <p>I love solving problems and making cool stuff with code.</p>
-              </div>
+ return (
+  <>
+   <div className='wrapper'>
+    <Head>
+     <title>Jordan Roberts</title>
+     <meta
+      property='og:title'
+      content='Jordan Roberts'
+     />
+     <meta
+      property='og:type'
+      content='website'
+     />
+     <meta
+      property='og:description'
+      content='Jordan Roberts, full stack web developer specializing in React'
+     />
+     <meta
+      property='og:image'
+      content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png'
+     />
+     <meta
+      property='og:url'
+      content='https://www.jrobertsweb.dev/'
+     />
+     <meta
+      name='twitter:card'
+      content='summary_large_image'
+     />
+     <meta
+      name='twitter:image'
+      content='https://i.postimg.cc/yNFbH1zg/og-portfolio.png'
+     />
+     <meta
+      name='twitter:creator'
+      content='@jRobertsWebDev'
+     />
+     <meta
+      name='viewport'
+      content='width=500'
+     />
+     <link
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/favicon.png'
+     />
+     {/* <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' /> */}
+    </Head>
+    <div className='container'>
+     <Header
+      dark={dark}
+      contactOpen={() => setContact(!contact)}
+      toggleDark={() => setDark(!dark)}
+      scrollTo={scrollTo}
+     />
+     <div className='boundary'>
+      <ModalWrap
+       dark={dark}
+       show={contact}
+       close={() => setContact(!contact)}
+      />
+     </div>
+     <Update />
+     <div className='mobile'>
+      <h1>Jordan Roberts</h1>
+      <h2>Full stack web developer</h2>
+     </div>
+     <div className='main'>
+      <div className='col bio'>
+       <h1 className='desktop'>Jordan Roberts</h1>
+       <h2 className='desktop'>Full stack web developer</h2>
+       <div className='text'>
+        <p>{bio}</p>
+        <p>I love solving problems and making cool stuff with code.</p>
+       </div>
 
-              <p className='email'>{email}</p>
-            </div>
-            <div className='col image'>
-              <span>{imageText}</span>
-            </div>
-          </div>
-        </div>
-
-        <Skills id='skills' dark={dark} />
-        <div id='projects' ref={projectsRef} />
-        <Projects dark={dark} />
-        <Footer dark={dark} />
+       <p className='email'>{email}</p>
       </div>
+      <div className='col image'>
+       <span>{imageText}</span>
+      </div>
+     </div>
+    </div>
 
-      <style jsx>
-        {`
-          .wrapper {
-            padding: 0.5rem 0.75rem 0.5rem 1.5rem;
-          }
-          .container {
-            padding-bottom: 10rem;
-            background-color: ${dark ? 'var(--clr-bg-d)' : 'var(--clr-bg-l)'};
-            width: 100%;
-            max-width: 1600px;
-            margin: 0 auto;
-            border: 2px solid ${dark ? 'var(--clr-text-dp)' : 'var(--clr-text-s)'};
-            border-radius: 55px;
-            transition: background-color 0.5s ease;
-          }
-          .main {
-            padding: 0 7%;
-            display: flex;
-            justify-content: center;
-          }
-          .col {
-            padding: 0 0 0 0;
-          }
+    <Skills
+     id='skills'
+     dark={dark}
+    />
+    <div
+     id='projects'
+     ref={projectsRef}
+    />
+    <Projects dark={dark} />
+    <Footer dark={dark} />
+   </div>
 
-          .bio {
-            width: 55%;
-          }
-          h1 {
-            font-family: var(--ff-primary);
-            font-weight: 500;
-            font-size: clamp(4rem, 2.7143rem + 2.2857vw, 5rem);
-            color: ${dark ? 'var(--clr-text-dp)' : 'var(--clr-text-p)'};
-          }
-          .desktop {
-            display: block;
-          }
+   <style jsx>
+    {`
+     .wrapper {
+      padding: 0.5rem 0.75rem 0.5rem 1.5rem;
+     }
+     .container {
+      padding-bottom: 10rem;
+      background-color: ${dark ? 'var(--clr-bg-d)' : 'var(--clr-bg-l)'};
+      width: 100%;
+      max-width: 1600px;
+      margin: 0 auto;
+      border: 2px solid ${dark ? 'var(--clr-text-dp)' : 'var(--clr-text-s)'};
+      border-radius: 55px;
+      transition: background-color 0.5s ease;
+     }
+     .main {
+      padding: 0 7%;
+      display: flex;
+      justify-content: center;
+     }
+     .col {
+      padding: 0 0 0 0;
+     }
 
-          h2 {
-            font-family: var(--ff-primary);
-            font-size: clamp(1.5rem, 0.2143rem + 2.2857vw, 2.5rem);
-            font-weight: 600;
-            color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
-          }
-          .text {
-            margin-block: 3rem;
-          }
-          p {
-            max-width: 400px;
-            font-family: var(--ff-second);
-            font-weight: 500;
-            font-size: 1.25rem;
-            margin-block: 1rem;
-            color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
-          }
-          .email {
-            font-weight: 400;
-            font-family: var(--ff-primary);
-            color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
-            opacity: ${dark ? '50%' : '75%'};
-          }
-          .image {
-            display: flex;
-            padding: 0;
-            align-items: center;
-          }
-          span {
-            font-family: var(--ff-rubik);
-            font-size: clamp(15rem, 2.1429rem + 22.8571vw, 25rem);
-            line-height: 1;
-            max-width: 900px;
-            margin: 0 auto;
-            padding-top: 2rem;
-            background-color: hsl(185, 100%, 49.2%);
-            background-image: var(--gradient);
-            background-size: 300% 300%;
-            background-clip: text;
+     .bio {
+      width: 55%;
+     }
+     h1 {
+      font-family: var(--ff-primary);
+      font-weight: 500;
+      font-size: clamp(4rem, 2.7143rem + 2.2857vw, 5rem);
+      color: ${dark ? 'var(--clr-text-dp)' : 'var(--clr-text-p)'};
+     }
+     .desktop {
+      display: block;
+     }
 
-            animation: gradient 5s ease infinite;
-            color: transparent;
-          }
-          .mobile {
-            display: none;
-          }
-          @keyframes gradient {
-            0% {
-              background-position: 0% 100%;
-            }
-            50% {
-              background-position: 100% 0%;
-            }
-            100% {
-              background-position: 0% 100%;
-            }
-          }
+     h2 {
+      font-family: var(--ff-primary);
+      font-size: clamp(1.5rem, 0.2143rem + 2.2857vw, 2.5rem);
+      font-weight: 600;
+      color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
+     }
+     .text {
+      margin-block: 3rem;
+     }
+     p {
+      max-width: 400px;
+      font-family: var(--ff-second);
+      font-weight: 500;
+      font-size: 1.25rem;
+      margin-block: 1rem;
+      color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
+     }
+     .email {
+      font-weight: 400;
+      font-family: var(--ff-primary);
+      color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
+      opacity: ${dark ? '50%' : '75%'};
+     }
+     .image {
+      display: flex;
+      padding: 0;
+      align-items: center;
+     }
+     span {
+      font-family: var(--ff-rubik);
+      font-size: clamp(15rem, 2.1429rem + 22.8571vw, 25rem);
+      line-height: 1;
+      max-width: 900px;
+      margin: 0 auto;
+      padding-top: 2rem;
+      background-color: hsl(185, 100%, 49.2%);
+      background-image: var(--gradient);
+      background-size: 300% 300%;
+      background-clip: text;
 
-          @media (max-width: 1250px) {
-            h1 {
-              font-size: 52px;
-            }
-            .bio {
-              min-width: 300px;
-              max-width: 500px;
-            }
-            .bio {
-              padding-top: 5%;
-            }
-          }
-          @media (max-width: 950px) {
-            .desktop {
-              display: none;
-            }
-            .mobile {
-              display: flex;
-              flex-direction: column;
-              padding: 4rem;
-              text-align: center;
-            }
-            .image {
-              align-items: flex-start;
-            }
+      animation: gradient 5s ease infinite;
+      color: transparent;
+     }
+     .mobile {
+      display: none;
+     }
+     @keyframes gradient {
+      0% {
+       background-position: 0% 100%;
+      }
+      50% {
+       background-position: 100% 0%;
+      }
+      100% {
+       background-position: 0% 100%;
+      }
+     }
 
-            .main {
-              gap: 1rem;
-            }
-            .col {
-              padding-top: 0%;
-            }
-            h1 {
-              font-size: 68px;
-            }
-            h2 {
-              font-size: 28px;
-            }
-          }
-          @media (max-width: 770px) {
-            .main {
-              flex-direction: column-reverse;
-            }
-            h1 {
-              margin-top: 3rem;
-              font-size: 4.5rem;
-              line-height: 1.25;
-            }
-            h2 {
-              margin-top: 2rem;
-              font-size: 2rem;
-              line-height: 1.25;
-            }
-            span {
-              max-width: 400px;
-              font-size: 250px;
-              padding: 0;
-            }
-            .bio {
-              width: 100%;
-              max-width: 100%;
-            }
-            p {
-              line-height: 1.5;
-              max-width: 90%;
-              margin: 2rem auto;
-              text-align: center;
-              font-size: 1.5rem;
-            }
-            .email {
-              margin-bottom: 2rem;
-            }
-            .text {
-              margin-top: 1rem;
-            }
-          }
-        `}
-      </style>
-      <style jsx global>
-        {`
-          body {
-            background-color: ${dark ? 'var(--clr-bg-d)' : 'var(--clr-bg-l)'};
-            transition: background-color var(--transition);
-          }
-          body::-webkit-scrollbar-track {
-            background-color: transparent;
-          }
+     @media (max-width: 1250px) {
+      h1 {
+       font-size: 52px;
+      }
+      .bio {
+       min-width: 300px;
+       max-width: 500px;
+      }
+      .bio {
+       padding-top: 5%;
+      }
+     }
+     @media (max-width: 950px) {
+      .desktop {
+       display: none;
+      }
+      .mobile {
+       display: flex;
+       flex-direction: column;
+       padding: 4rem;
+       text-align: center;
+      }
+      .image {
+       align-items: flex-start;
+      }
 
-          body::-webkit-scrollbar-thumb {
-            background-color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
+      .main {
+       gap: 1rem;
+      }
+      .col {
+       padding-top: 0%;
+      }
+      h1 {
+       font-size: 68px;
+      }
+      h2 {
+       font-size: 28px;
+      }
+     }
+     @media (max-width: 770px) {
+      .main {
+       flex-direction: column-reverse;
+      }
+      h1 {
+       margin-top: 3rem;
+       font-size: 4.5rem;
+       line-height: 1.25;
+      }
+      h2 {
+       margin-top: 2rem;
+       font-size: 2rem;
+       line-height: 1.25;
+      }
+      span {
+       max-width: 400px;
+       font-size: 250px;
+       padding: 0;
+      }
+      .bio {
+       width: 100%;
+       max-width: 100%;
+      }
+      p {
+       line-height: 1.5;
+       max-width: 90%;
+       margin: 2rem auto;
+       text-align: center;
+       font-size: 1.5rem;
+      }
+      .email {
+       margin-bottom: 2rem;
+      }
+      .text {
+       margin-top: 1rem;
+      }
+     }
+    `}
+   </style>
+   <style
+    jsx
+    global>
+    {`
+     body {
+      background-color: ${dark ? 'var(--clr-bg-d)' : 'var(--clr-bg-l)'};
+      transition: background-color var(--transition);
+     }
+     body::-webkit-scrollbar-track {
+      background-color: transparent;
+     }
 
-            border-radius: 5px;
-          }
-          body::-webkit-scrollbar {
-            width: 0.75rem;
-          }
-        `}
-      </style>
-    </>
-  )
+     body::-webkit-scrollbar-thumb {
+      background-color: ${dark ? 'var(--clr-text-ds)' : 'var(--clr-text-s)'};
+
+      border-radius: 5px;
+     }
+     body::-webkit-scrollbar {
+      width: 0.75rem;
+     }
+    `}
+   </style>
+  </>
+ )
 }
 
 // export async function getStaticProps(ctx) {
